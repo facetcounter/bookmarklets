c25329f6da2bb26a6f0a4ea205f2e69f3a3e1ef1
Nathan Young <facetcounter@gmail.com>
2011-10-31 15:03:47 -0700

    adding stuff for wikify bookmarklet

---
+++ wikify/wikify.js
@@ -0,0 +1,89 @@
+(function(){
+
+	var o=document.createElement("script");
+	document.getElementsByTagName("head")[0].appendChild(o);
+	o.src='http://code.jquery.com/jquery-1.4.1.min.js';void(0);
+
+})();
+
+(function(){
+
+	var pollJquery = function(){
+		if (typeof jQuery == 'undefined'){
+			setTimeout(pollJquery,500);
+		}
+		else {
+			doOncePresent();
+		}
+	};
+	setTimeout(pollJquery,500);
+
+	var doOncePresent = function(){
+		jQuery.getScript('http://www.ben-daglish.net/textile/textile.js',function(){
+			jQuery('head').append(
+				'<style>body {font-family:Arial,Helvetica,sans-serif;font-size:10pt;}\n'+
+					'#preview {border:1px black solid;height:90%;overflow:scroll;}\n'+
+					'#tedit {float:left;width:50%;height:90%;}\n'+
+					'#textile {width:100%;height:100%;}\n'+
+					'#htmlp {border:1px black solid;font-size:8pt;}\n'+
+					'#tabs a {display:inline-block;width:80px;border:1px black solid;padding:3px;}\n'+
+					'.biggreen {color:green;font-size:1.2em;}\n'+
+					'#italic {font-style:italic;}\n'+
+				'</style>'
+			);
+			var wikiContents = jQuery('body')[0].innerHTML;
+			jQuery('body')[0].innerHTML="";
+			jQuery('body').append(
+				'<a id="saveMe" href="#">save</a><br/>\n'+
+				'<form>\n'+
+				'<div id="tedit">\n'+
+				'<textarea id="textile" name="txtl" rows="20" cols="50">\n'+
+				wikiContents +
+				'</textarea>\n'+
+				'</div>\n'+
+				'</form>\n'+
+				'<div id="preview"></div>\n'+
+				'<div id="htmlp"></div>\n'
+			);
+			jQuery('#saveMe').click(saveMe);
+			jQuery('#textile').keyup(doConvert);
+			doConvert();
+		});
+
+
+		var doConvert = function() {
+			var html = convert(document.getElementById('textile').value);
+			document.getElementById('preview').innerHTML=html;
+			html = html.replace(/&/g,"&amp;").replace(/</g,"&lt;");
+			document.getElementById('htmlp').innerHTML=tag("pre",'',html);
+		};
+
+		var saveMe = function () {
+			if(window.Components) {
+				var filePath = window.location.pathname;
+				filePath = filePath.substring(1);
+				filePath = filePath.replace(/\//g,'\\');
+
+				var content = jQuery("#textile").val();
+				//try {
+					netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+					var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+					console.log(filePath);
+					file.initWithPath(filePath);
+					if(!file.exists())
+						file.create(0,0x01B4);// 0x01B4 = 0664
+					var out = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
+					out.init(file,0x22,0x04,null);
+					out.write(content,content.length);
+					out.flush();
+					out.close();
+					jQuery("#saveMe").after("saved!");
+				//} catch(ex) {
+				//	return false;
+				//}
+			}
+			return null;
+		};
+
+	};
+})();
